ba9a086abe8274617437892fcf3fa401a6a61153
droidkos <droidkos@gmail.com>
2021-09-07 09:59:15 +0300

    cleaner fix

--- src/cleaner.py
+++ src/cleaner.py
@@ -1,9 +1,9 @@
 import os
 
 
-for folder, subfolder, filelist in os.walk('.\sliced'):
-    if len(filelist) > 1:
-        for file in filelist:
-            if int(file[:3]) % 2 == 0:
-                # print('file to delete:', os.path.join(folder, file))
-                os.remove(os.path.join(folder, file))
+if __name__ == '__main__':
+    for folder, subfolder, filelist in os.walk('.\sliced'):
+        if len(filelist) > 1:
+            for file in filelist:
+                if int(file[:3]) % 2 == 0:
+                    os.remove(os.path.join(folder, file))
