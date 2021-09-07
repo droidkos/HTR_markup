1a156d213389bd0a20e08ad110fa91d4a70f16c3
droidkos <droidkos@gmail.com>
2021-09-07 15:26:52 +0300

    rewrote cleaner

--- src/cleaner.py
+++ src/cleaner.py
@@ -1,9 +1,17 @@
 import os
 
 
-if __name__ == '__main__':
-    for folder, subfolder, filelist in os.walk('.\sliced'):
+def cleaner(folder):
+    """
+    :param folder: папка, где чистим
+    Функция убирает изображения с печатным текстом из папки с нерезанными изображениями
+    """
+    for folder, subfolder, filelist in os.walk(folder):
         if len(filelist) > 1:
             for file in filelist:
                 if int(file[:3]) % 2 == 0:
                     os.remove(os.path.join(folder, file))
+
+
+if __name__ == '__main__':
+    cleaner('.\sliced')
